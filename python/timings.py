11b25584dc45b57ab7a787fc3cf84d68547a1881
Adam Amin <adamamin54@gmail.com>
2026-07-24 16:18:29 -0700

    chore: small updates to python code

--- python/timings.py
+++ python/timings.py
@@ -1,25 +1,34 @@
 import time
+import math
 
 ''' Example: 4/4 - 1 chords per bar'''
+
+def get_frequency(bpm: int) -> float:
+    return (1 / (bpm / 60))
 
 
 def main():
     bpm: int = int(input('BPM: '))
-    beats: int = int(input('Beats'))
-    note_length: int = int(input('Duration'))
-    # chords_per_bar
-    # music_time = f'{beats}/{duration}'
-    freq = (1 / (bpm / 60)) * beats
+    note_length: int = int(input('Duration: '))
+
+    frequency = get_frequency(bpm)
+
+    if note_length == 0 or math.pow(2,note_length) % note_length != 0:
+        raise ValueError("Note length must be a valid note duration")
+
+    wait_time = frequency * note_length
+
+    print(f'{frequency}: {wait_time}')
 
     fb = True
-    while 1:
-        if fb is True:
-            print("True")
-            fb = False
-        else:
-            print("False")
-            fb = True
-        time.sleep(freq)
+    # while 1:
+    #     if fb is True:
+    #         print("True")
+    #         fb = False
+    #     else:
+    #         print("False")
+    #         fb = True
+    #     time.sleep(freq)
 
 
 def sleepTime():
