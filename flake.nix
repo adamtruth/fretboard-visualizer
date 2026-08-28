27b2dc37a858388f9c9deefbf59559c0f9744f0d
Adam Amin <adamamin54@gmail.com>
2026-08-28 14:27:10 -0700

    chore: update flake to python 3.14

--- flake.nix
+++ flake.nix
@@ -18,20 +18,17 @@
           packages = with pkgs; [
             bashInteractive
             uv
-            python3
-            gcc
-            linuxHeaders
+            pyright
+
             (python3.withPackages (ps: with ps; [
               tkinter
               evdev
               pillow
             ]))
           ];
-          # Explicitly tell setup.py where the header files are stored in Nix
-          C_INCLUDE_PATH = "${pkgs.linuxHeaders}/include:${pkgs.alsa-lib}/include";
 
           shellHook = ''
-            echo "Nix build environment loaded with Linux headers!"
+            echo "$(python3 --version)"
           '';
         };
       }
